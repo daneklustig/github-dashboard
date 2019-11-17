f918fa24de51d9a519c34cc98e0e9eef667ae9a2
daneklustig <danek.lustig@gmail.com>
2019-11-17 16:17:29 +0100

    navbar

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,17 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index', { loggedIn: req.user});
 });
 
-router.get('/')
+const loginCheck = () => {
+  return (req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect("/");
+    }
+  };
+};
 
 module.exports = router;
