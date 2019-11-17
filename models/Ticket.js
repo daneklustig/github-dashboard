c66276e6c3c565c0b9ff4930631182d5ca9a2a3f
crlybrcs <tatyana_@outlook.com>
2019-11-17 16:55:04 +0100

    added Ticket schema

---
+++ models/Ticket.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ticketSchema = new Schema({
+  availableFrom: Date,
+  availableUntil: Date,
+  zone: {
+    type: String,
+    enum: ["AB", "ABC"]
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+const Ticket = mongoose.model("Ticket", ticketSchema);
+
+module.exports = Ticket;
